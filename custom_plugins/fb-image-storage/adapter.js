89d87a13d7cf8b8a7a5d4f7b3359ece97858aca4
Jacob Johnston <jacob@flight.run>
2018-10-12 14:15:43 -0700

    moving all initialization inside class constructor

--- custom_plugins/fb-image-storage/adapter.js
+++ custom_plugins/fb-image-storage/adapter.js
@@ -1,22 +1,26 @@
 import firebase from 'firebase/app';
 import 'firebase/storage'
 
-const app = firebase.initializeApp({
-	apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
-	authDomain: 'cosourcerytest.firebaseapp.com',
-	databaseURL: 'https://cosourcerytest.firebaseio.com',
-	projectId: 'cosourcerytest',
-	storageBucket: 'cosourcerytest.appspot.com',
-	messagingSenderId: '146479623747'
-});
 
-const storage = app.storage();
-
-const ref = storage.ref();
 
 export default class Adapter {
 
 	constructor(storagePath, loader, t) {
+		const app = firebase.initializeApp({
+			apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
+			authDomain: 'cosourcerytest.firebaseapp.com',
+			databaseURL: 'https://cosourcerytest.firebaseio.com',
+			projectId: 'cosourcerytest',
+			storageBucket: 'cosourcerytest.appspot.com',
+			messagingSenderId: '146479623747'
+		});
+
+		const storage = app.storage();
+
+		const ref = storage.ref();
+
+		this.ref = ref;
+
 		this.loader = loader;
 		// this.fbConfig = fbConfig;
 		this.t = t;
@@ -37,15 +41,14 @@ export default class Adapter {
 
 	_sendFile(resolve, reject) {
 		const file = this.loader.file;
-		console.log(file);
-		this.uploadTask = ref.child(this.storagePath).child(file.name).put(file);
+		this.uploadTask = this.ref.child(this.storagePath).child(file.name).put(file);
 		this.uploadTask.on('state_changed', (snapshot) => {
 			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 			this.loader.uploadTotal = progress;
 		}, (err) => {
 			return reject(err);
 		}, () => {
-			this.uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+			this.uploadTask.snapshot.this.ref.getDownloadURL().then((url) => {
 				resolve({
 					default: url
 				});
