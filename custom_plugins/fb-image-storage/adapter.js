de2a48b61de7edd6d23ac6a6c71591e1b0eb33b8
Jacob Johnston <jacob@flight.run>
2018-10-12 14:25:02 -0700

    What if I just pass in a ready storage reference...

--- custom_plugins/fb-image-storage/adapter.js
+++ custom_plugins/fb-image-storage/adapter.js
@@ -1,32 +1,26 @@
-import firebase from 'firebase/app';
-import 'firebase/storage'
+// import firebase from 'firebase/app';
+// import 'firebase/storage'
 
+// const app = firebase.initializeApp({
+// 	apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
+// 	authDomain: 'cosourcerytest.firebaseapp.com',
+// 	databaseURL: 'https://cosourcerytest.firebaseio.com',
+// 	projectId: 'cosourcerytest',
+// 	storageBucket: 'cosourcerytest.appspot.com',
+// 	messagingSenderId: '146479623747'
+// });
 
-
+// const storage = app.storage();
+// const ref = storage.ref('articleBodyImages/');
 export default class Adapter {
 
-	constructor(storagePath, loader, t) {
-		const app = firebase.initializeApp({
-			apiKey: 'AIzaSyAb3L-t-WB0rf6A9j8gVSRB9STJJvLUEfw',
-			authDomain: 'cosourcerytest.firebaseapp.com',
-			databaseURL: 'https://cosourcerytest.firebaseio.com',
-			projectId: 'cosourcerytest',
-			storageBucket: 'cosourcerytest.appspot.com',
-			messagingSenderId: '146479623747'
-		});
-
-		const storage = app.storage();
-
-		const ref = storage.ref();
-
-		this.ref = ref;
-
+	constructor(storageRef, loader, t) {
+		this.storageRef = storageRef;
 		this.loader = loader;
-		// this.fbConfig = fbConfig;
 		this.t = t;
 		// this.app = firebase.initializeApp(this.fbConfig);
 		// this.storage = app.storage();
-		this.storagePath = storagePath || 'imageUploads/';
+		// this.ref = storage.ref(storagePath);
 	}
 
 	upload() {
@@ -41,14 +35,15 @@ export default class Adapter {
 
 	_sendFile(resolve, reject) {
 		const file = this.loader.file;
-		this.uploadTask = this.ref.child(this.storagePath).child(file.name).put(file);
+
+		this.uploadTask = this.storageRef.put(file);
 		this.uploadTask.on('state_changed', (snapshot) => {
 			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 			this.loader.uploadTotal = progress;
 		}, (err) => {
 			return reject(err);
 		}, () => {
-			this.uploadTask.snapshot.this.ref.getDownloadURL().then((url) => {
+			this.uploadTask.snapshot.ref.getDownloadURL().then((url) => {
 				resolve({
 					default: url
 				});
