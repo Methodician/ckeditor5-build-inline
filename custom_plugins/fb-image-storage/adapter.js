df82767fea1ca0aea7237209fca063fc64f9a871
Jacob Johnston <jacob@flight.run>
2018-11-03 13:46:31 -0700

    testing search for additional errors

--- custom_plugins/fb-image-storage/adapter.js
+++ custom_plugins/fb-image-storage/adapter.js
@@ -8,7 +8,12 @@ export default class Adapter {
 
 	upload() {
 		return new Promise((resolve, reject) => {
-			this._sendFile(resolve, reject);
+			try {
+				this._sendFile(resolve, reject);
+			} catch (error) {
+				console.log('error in upload tryCatch', error);
+				return reject(error);
+			}
 		});
 	}
 
@@ -24,6 +29,7 @@ export default class Adapter {
 			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 			this.loader.uploadTotal = progress;
 		}, (err) => {
+			console.log('error in _sendFile uploadTask.on()', err);
 			return reject(err);
 		}, () => {
 			this.uploadTask.snapshot.ref.getDownloadURL().then((url) => {
